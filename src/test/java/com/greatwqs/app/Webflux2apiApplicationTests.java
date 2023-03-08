bb722582ee362989ce1eb149cfea777c83e686f9
wang-qingsong <wang.qingsong@navercorp.com>
2023-03-08 11:21:44 +0800

    INIT package and mvn config

---
+++ src/test/java/com/greatwqs/app/Webflux2apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatwqs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux2apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
